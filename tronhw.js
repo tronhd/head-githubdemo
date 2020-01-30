3ae72cc3d7861c32995fbf82734571c2e6ef35a3
melvasquires <melva.a.squires@gmail.com>
2020-01-30 17:13:11 -0500

    some hw incas of crash again

--- tronhw.js
+++ tronhw.js
@@ -3,7 +3,55 @@ var number = 1;
 console.log(x * number);
 console.log(4 + 7);
 console.log(2 + true);
-con
+
+//CONDITINAL
+
+var count= 2;
+var result;
+var answer;
+var count1= count + 5;
+var count2= (count + 5) * 2;
+
+console.log(count2)
+
+// the answer should be 2
+
+var count3= ((count + 5) * 2) % 3;
+
+console.log(count3)
+
+//there shouldnt be a remainder or 0
+
+var count4= (((count + 5) * 2) % 3) % 2;
+
+console.log(count4)
+
+if(count4 === 0){
+    result = "it's right because count4 is 0";
+} else if(count4 > 0) {
+    result = "excuse me what";
+} else
+    result = "undefined";
+
+if(count4 >= 0){
+    answer = "count is any integer";
+} else 
+console.log(answer)
+
+//LOOP
+
+var th
+    for(th= 0; th<50;th++) {
+        console.log(th);
+    }
+
+var count= 0;
+    while(count < 50){
+      if(count % 2== 0)
+        console.log(count);
+        count++;
+    }        
+
 
 
 

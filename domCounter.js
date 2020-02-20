f57c129307e916ff84e5142f116c5e0a76ca6886
tronhd <melva.a.squires@gmail.com>
2020-02-20 16:47:59 -0500

    updated domCount

--- domCounter.js
+++ domCounter.js
@@ -1,7 +1,14 @@
-var down= document.getElementById("down"),
-  count = 0;
-
-    down.onclick = function() {
-      count -= 4;
-      down.innerHTML = "Going Down: " + count;
+var image= document.getElementById("image");
+var down= document.getElementById("down");
+  count = 50;
+ down.onclick = function() {
+      if(count >= 5){
+          count -= 5;
+      } else if(count == 0){
+        image.style.display= 'block';
+        //else isnt realy working but its in there
+      }else{
+          console.log(alert("Stop Clicking")),
+            console.log("Seriously");
+      }down.innerHTML = "Click Here: " + count; 
     };
